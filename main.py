6a0abe09606b2824d9a301486b1e0ea22e1580d8
thhonigm <thonigmann@web.de>
2025-04-23 23:23:04 +0200

    Ch2 L6

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
+from stats import word_count, letter_count
 
-def word_count(text):
-    return len(text.split())
-
-def letter_count(text):
-    count = {}
-    for c in text.lower():
-        try:
-            count[c] += 1
-        except KeyError:
-            count[c] = 1
-    return count
-
-def ordered_letter_count(text):
-    count = []
-    for c,n in letter_count(text).items():
-        count.append((c,n))
-    count.sort(key = lambda x: x[1], reverse = True)
-    return count
+def main1():
+    path ="books/frankenstein.txt"
+    with open(path) as f:
+        file_contents = f.read()
+    print(f"{word_count(file_contents)} words found in the document")
+    print(letter_count(file_contents))
 
 def main():
     path ="books/frankenstein.txt"
@@ -31,5 +20,5 @@ def main():
             print(f"The '{c}' character was found {n} times")
     print(f"--- End report ---")
 
-main()
 
+main1()
---
+++ stats.py
@@ -0,0 +1,20 @@
+
+def word_count(text):
+    return len(text.split())
+
+def letter_count(text):
+    count = {}
+    for c in text.lower():
+        try:
+            count[c] += 1
+        except KeyError:
+            count[c] = 1
+    return count
+
+def ordered_letter_count(text):
+    count = []
+    for c,n in letter_count(text).items():
+        count.append((c,n))
+    count.sort(key = lambda x: x[1], reverse = True)
+    return count
+
